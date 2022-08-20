ed17e2a11e6ac51e95c9d894abafd52eee403414
speeuh <lu.alves306@hotmail.com>
2022-08-20 01:28:59 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { loadPokemons } from './api';
+import Navbar from './components/navbar';
+
+import styles from './App.module.scss';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    const result = await loadPokemons();
+    setPokemons(result);
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+    console.log(pokemons);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.page}>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Navbar.module.scss';
+
+export default function Navbar() {
+
+    return (
+        <div className={styles.icon}>
+            <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="pokeapi" />
+        </div>
+    )
+}
